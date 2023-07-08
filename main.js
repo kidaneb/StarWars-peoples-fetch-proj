710ecf7d42e163ed6514adf7d52d0404a0eba98a
kidaneb <105313373+kidaneb@users.noreply.github.com>
2023-07-08 10:19:23 +0300

    resturaturing the position

--- main.js
+++ main.js
@@ -1,36 +1,51 @@
 const mainContainer = document.getElementById('main-container');
 const container = document.getElementById('container');
 const container_ = document.querySelector('.container_');
+const loading = document.getElementById('loading');
 
 
 fetch('https://swapi.dev/api/people')
-.then(response => response.json())
-.then(data => {
-    
-    data.results.forEach(person =>{
-        const container = document.createElement('div');
-        container.className ='container';
-        container.innerHTML = 
-                                    `
-                                    
-                                    <div class="people">
-                                        <div>
-                                            <p>Name:${person.name}</p>
-                                            <p>Height:${person.height}</p>
-                                            <p>Mass:${person.mass}</p>
-                                        </div>
-                                        <div>
-                                            <p>Hair_color:${person.hair_color}</p>
-                                            <p>Skin_color:${person.skin_color}</p>
-                                        </div>  
-
-                                    </div>
-                                                            
-                                    `
-                        mainContainer.insertBefore(container, mainContainer.firstChild);
-    })
-    
+.then((response) => {
+    if (response.ok) {
+        response
+            .json()
+            .then((data) => {
+                data.results.forEach(person =>{
+                    loading.style.display = 'none';
+                    const container = document.createElement('div');
+                    container.className ='container';
+                    container.innerHTML = 
+                                                `
+                                                
+                                                <div class="people">
+                                                    <div>
+                                                        <p>Name:${person.name}</p>
+                                                        <p>Height:${person.height}</p>
+                                                        <p>Mass:${person.mass}</p>
+                                                    </div>
+                                                    <div>
+                                                        <p>Hair_color:${person.hair_color}</p>
+                                                        <p>Skin_color:${person.skin_color}</p>
+                                                    </div>  
+            
+                                                </div>
+                                                                        
+                                                `
+                                    mainContainer.insertBefore(container, mainContainer.firstChild);
+                })
+            })
+    }
+    else{
+        console.log('Error fetching data from the API')
+    }
 })
+.catch((error) => {
+    loading.innerHTML = 'ERROR JUST HAPPENDED';
+    loading.style.color = 'red';
+    
+
+} )
+
 
 
 
